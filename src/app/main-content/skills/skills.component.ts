58b8106332b6df7821b7edf45fbc8ae920195cce
matt-e-dev <dmdmlbwski@gmail.com>
2025-03-21 09:37:51 +0100

    skills first draft finished

--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
+
+  skills = [
+    { name: 'HTML', icon: 'assets/icons/html.png' },
+    { name: 'CSS', icon: 'assets/icons/css.png' },
+    { name: 'JavaScript', icon: 'assets/icons/javascript.png' },
+    { name: 'Material Design', icon: 'assets/icons/material-design.png' },
+    { name: 'TypeScript', icon: 'assets/icons/typescript.png' },
+    { name: 'Angular', icon: 'assets/icons/angular.png' },
+    { name: 'Firebase', icon: 'assets/icons/firebase.png' },
+    { name: 'GIT', icon: 'assets/icons/git.png' },
+    { name: 'Rest-API', icon: 'assets/icons/rest-api.png' },
+    { name: 'Scrum', icon: 'assets/icons/scrum.png' },
+    { name: 'Growth mindset', icon: 'assets/icons/growth-mindset.png' }
+  ];
 
 }

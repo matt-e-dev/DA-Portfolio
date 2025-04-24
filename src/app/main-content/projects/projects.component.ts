0f95cdcabe2f56d3d693386b99b4f8dfe18bd5f5
matt-e-dev <dmdmlbwski@gmail.com>
2025-04-24 08:15:36 +0200

    working on overlay for projects

--- src/app/main-content/projects/projects.component.ts
+++ src/app/main-content/projects/projects.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
@@ -21,5 +22,18 @@ export class ProjectsComponent {
     if (element) {
       element.classList.toggle('d-none');
     }
+  }
+
+  isOverlayOpen = false;
+  selectedProject = '';
+
+  openOverlay(projectName: string): void {
+    this.isOverlayOpen = true;
+    this.selectedProject = projectName;
+  }
+
+  closeOverlay(): void {
+    this.isOverlayOpen = false;
+    this.selectedProject = '';
   }
 }

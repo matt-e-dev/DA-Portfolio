a08630ca75f4d0defc22700bd342a03e462c39f1
matt-e-dev <dmdmlbwski@gmail.com>
2025-05-06 19:12:50 +0200

    code formatted with prettier

--- src/app/main-content/projects/projects.component.ts
+++ src/app/main-content/projects/projects.component.ts
@@ -6,24 +6,23 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
-
   toggleImage(projectId: string, show: boolean): void {
     const element = document.getElementById(projectId);
     if (element) {
       if (show) {
-        element.classList.remove('d-none'); 
+        element.classList.remove('d-none');
       } else {
-        element.classList.add('d-none'); 
+        element.classList.add('d-none');
       }
     }
   }
 
-  isOverlayOpen = false; 
-  
-  selectedProject: { 
+  isOverlayOpen = false;
+
+  selectedProject: {
     id: string;
     count: string;
     title: string;
@@ -31,10 +30,10 @@ export class ProjectsComponent {
     description: string;
     technologies: string;
     image: string;
-    skills: { name: string; icon: string }[]; 
+    skills: { name: string; icon: string }[];
   } | null = null;
 
-  selectedProjectIndex: number = 0; 
+  selectedProjectIndex: number = 0;
 
   changeSelectedProject(index: number): void {
     this.selectedProjectIndex = index;
@@ -46,7 +45,8 @@ export class ProjectsComponent {
       count: '01',
       title: 'JOIN',
       question: 'What is the project about?',
-      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      description:
+        'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
       technologies: 'Angular | TypeScript | HTML | CSS | Firebase',
       image: 'assets/imgs/test1.png',
       icon: 'assets/icons/join-icon.svg',
@@ -55,74 +55,74 @@ export class ProjectsComponent {
         { name: 'TypeScript', icon: 'assets/icons/projects/typescript.svg' },
         { name: 'HTML', icon: 'assets/icons/projects/html.svg' },
         { name: 'CSS', icon: 'assets/icons/projects/css.svg' },
-        { name: 'Firebase', icon: 'assets/icons/projects/firebase.svg' }
-      ]
+        { name: 'Firebase', icon: 'assets/icons/projects/firebase.svg' },
+      ],
     },
     {
       id: 'elPolloLoco',
       count: '02',
       title: 'El Pollo Loco',
       question: 'What is the project about?',
-      description: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+      description:
+        'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
       technologies: 'JavaScript | HTML | CSS',
       image: 'assets/imgs/test2.png',
       icon: 'assets/icons/el-pollo-loco-icon.svg',
       skills: [
         { name: 'JavaScript', icon: 'assets/icons/projects/javascript.png' },
         { name: 'HTML', icon: 'assets/icons/projects/html.png' },
-        { name: 'CSS', icon: 'assets/icons/projects/css.png' }
-      ]
-    }
-    
+        { name: 'CSS', icon: 'assets/icons/projects/css.png' },
+      ],
+    },
   ];
 
   nextProject(): void {
     if (this.selectedProject) {
-     
-      const currentIndex = this.projects.findIndex(project => project.id === this.selectedProject?.id);
-  
+      const currentIndex = this.projects.findIndex(
+        (project) => project.id === this.selectedProject?.id
+      );
+
       const nextIndex = (currentIndex + 1) % this.projects.length;
-  
+
       this.selectedProject = this.projects[nextIndex];
     }
   }
 
   openOverlay(projectId: string): void {
-    this.selectedProject = this.projects.find(project => project.id === projectId) || null;
+    this.selectedProject =
+      this.projects.find((project) => project.id === projectId) || null;
     this.isOverlayOpen = true;
-  
+
     document.body.classList.add('no-scroll');
   }
-  
+
   closeOverlay(): void {
     this.isOverlayOpen = false;
     this.selectedProject = null;
-  
+
     document.body.classList.remove('no-scroll');
   }
 
   hoverArrows(projectId: string): void {
     const arrowHoverRef = document.getElementById(`arrowHover-${projectId}`);
-    arrowHoverRef?.classList.toggle('d-none'); 
+    arrowHoverRef?.classList.toggle('d-none');
   }
 
-
   handleMouseLeave(projectId: string): void {
-    this.toggleImage(projectId, false); 
+    this.toggleImage(projectId, false);
     const arrowHoverRef = document.getElementById(`arrowHover-${projectId}`);
-    arrowHoverRef?.classList.add('d-none'); 
+    arrowHoverRef?.classList.add('d-none');
   }
 
   handleMouseOver(projectId: string): void {
     this.toggleImage(projectId, true);
     this.hoverArrows(projectId);
-  
-    const index = this.projects.findIndex(project => project.id === projectId);
+
+    const index = this.projects.findIndex(
+      (project) => project.id === projectId
+    );
     if (index !== -1) {
       this.selectedProjectIndex = index;
     }
   }
-  
-
-
 }

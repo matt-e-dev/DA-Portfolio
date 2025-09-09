dfa9fd643963c5a3035c2f076cdd1e1e032e1f3b
matt-e-dev <dmdmlbwski@gmail.com>
2025-09-09 14:13:05 +0200

    cursor shadow added

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+document.addEventListener('mousemove', (e) => {
+  const shadow = document.getElementById('cursor-shadow');
+  if (shadow) {
+    shadow.style.left = `${e.clientX}px`;
+    shadow.style.top = `${e.clientY}px`;
+  }
+});
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);

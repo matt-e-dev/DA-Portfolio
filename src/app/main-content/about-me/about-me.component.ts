57d3c7c2ceca489532f897088051ed7f68c53102
matt-e-dev <dmdmlbwski@gmail.com>
2025-09-09 13:21:55 +0200

    about me minor fix

--- src/app/main-content/about-me/about-me.component.ts
+++ src/app/main-content/about-me/about-me.component.ts
@@ -8,4 +8,15 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss',
 })
-export class AboutMeComponent {}
+export class AboutMeComponent {
+  showDeco() {
+    const deco = document.getElementById('picture_deco_container');
+    deco?.classList.toggle('d-none');
+  }
+
+  hideDeco() {
+    const deco = document.getElementById('picture_deco_container');
+    deco?.classList.add('d-none');
+  }
+}
+

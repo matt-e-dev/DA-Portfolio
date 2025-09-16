a79087aae36fe8d0d6373886c0d26056e5c90d6f
matt-e-dev <dmdmlbwski@gmail.com>
2025-09-16 08:47:07 +0200

    changed routes and working on legal-notice

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainContentComponent }, // default route
+  { path: 'legal-notice', component: LegalNoticeComponent },
+];

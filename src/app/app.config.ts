4feac0a05e9eb5f26e819ca9c779edb1c8d854f8
matt-e-dev <dmdmlbwski@gmail.com>
2025-11-01 10:57:49 +0100

    deprication error fixed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,7 +11,7 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     importProvidersFrom(
       TranslateModule.forRoot({
-        defaultLanguage: 'en',
+        fallbackLang: 'en',
       })
     ),
   ],

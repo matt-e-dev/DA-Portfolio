db8dcac60ec3b7a9a3cb23bdf3c587b77881467e
matt-e-dev <dmdmlbwski@gmail.com>
2025-11-14 12:24:06 +0100

    update

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })

016c412f66f42e4e7ec8f176b8acfa1bac8864b8
matt-e-dev <dmdmlbwski@gmail.com>
2025-11-20 11:12:45 +0100

    privacy-policy added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { MainContentComponent } from './main-content/main-content.component';
 
 export const routes: Routes = [
   { path: '', component: MainContentComponent }, // default route
   { path: 'legal-notice', component: LegalNoticeComponent },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent },
 ];
